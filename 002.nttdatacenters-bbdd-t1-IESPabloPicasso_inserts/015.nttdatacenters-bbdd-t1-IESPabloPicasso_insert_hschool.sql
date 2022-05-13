2a729bc5cbdbadf37eaf45a1e9428274f3e6787b
AdrianCaMu <a_adrian.camara.munoz@iespablopicasso.es>
2022-05-13 10:00:46 +0200

    insert modify

--- 002.nttdatacenters-bbdd-t1-IESPabloPicasso_inserts/015.nttdatacenters-bbdd-t1-IESPabloPicasso_insert_hschool.sql
+++ 002.nttdatacenters-bbdd-t1-IESPabloPicasso_inserts/015.nttdatacenters-bbdd-t1-IESPabloPicasso_insert_hschool.sql
@@ -4,7 +4,7 @@ use education;
 -- Inserción de los Institutos
 insert into high_school(high_school_id, name, province, address, type)
 values
-	('1', 'IES PABLO PICASSO', 'MALAGA', 'CALLE NOLOSE', 'public'),
-    ('2', 'CESUR MALAGA', 'MALAGA', 'CALLE TAMPOCO', 'private'),
-    ('3', 'CESUR SEVILLA', 'SEVILLA', 'CALLE NIIDEA', 'private'),
-    ('4', 'IES VEDRUNA', 'SEVILLA', 'CALLE YOQUESE', 'public');
+	('1', 'IES PABLO PICASSO', 'MALAGA', 'Cam. Castillejos, 8', 'public'),
+    ('2', 'CESUR MALAGA', 'MALAGA', 'C. Franz Liszt, 1', 'private'),
+    ('3', 'CESUR SEVILLA', 'SEVILLA', 'Estadio Olímpico, Isla de La Cartuja', 'private'),
+    ('4', 'IES VEDRUNA', 'SEVILLA', ' C. Espinosa y Cárcel, 21', 'public');
